5726794f16071f5428f14b15459a20316463ac7f
瞿星 <xing.qux@alibaba-inc.com>
2014-06-12 11:33:16 +0800

    改为由js生成，不依赖接口

--- src/main.js
+++ src/main.js
@@ -6,8 +6,15 @@ try {
             console.log(url);
             //var iframe = document.getElementById('qrcode');
             //iframe.src = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&choe=UTF-8&chld=L|4&chl=" + url;
+            /*
             var img = document.getElementById('qrcode');
             img.src = "http://qr.liantu.com/api.php?text=" + url;
+            */
+            $("#qrcode").qrcode({
+                width: 200,
+                height: 200,
+                text: tab.url
+            });
     });
 
 }catch(e){
